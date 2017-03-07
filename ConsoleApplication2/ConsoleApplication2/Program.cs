fe6e9df2238351d263d8b377f7998f30c6c2a0bb
errorv <szmagajjakub@gmail.com>
2017-03-07 15:34:31 +0100

    cw1

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            samochod x = new samochod("BMW", 4, 0);
+            samochod y = new samochod("Szerszen", 4, 100, "pomaranczowy", "1989");
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.ReadKey();
+
+            x.Jedz(140);
+            y.Jedz(80);
+
+            
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.ReadKey();
+
+            x.Hamuj();
+            y.Hamuj();
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ConsoleApplication2/ConsoleApplication2/samochod.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class samochod
+    {
+        public string Marka { get; private set; }
+        public int LiczbaKol { get; private set; }
+        public int Predkosc { get; private set; }
+        public string Kolor { get; private set; }
+        public string Rocznik { get; private set; }
+        public samochod(string marka, int lk, int v, string kolor="Czarny", string r="2016")
+        {
+            Marka = marka;
+            LiczbaKol = lk;
+            Predkosc = v;
+            Kolor = kolor;
+            Rocznik = r;
+        }
+        public void Hamuj()
+        {
+            Predkosc = 0;
+            Console.WriteLine("samochod {0} koloru {1} zatrzymal sie", Marka, Kolor);
+        
+        }
+        public void Jedz(int v)
+        {
+            if (v > 0)
+                Predkosc = v;
+          Console.WriteLine("samochod {0} koloru {1} ruszyl z predkoscia {2}", Marka, Kolor, Predkosc);
+        }
+        public override string ToString()
+        {
+            return string.Format("{0} samochód marki {1} jedzie z prędkością {2}"
+                                 + "\nDomyślna ilość kół używanych jednocześnie w samochodzie to {3}\n",
+                                 Kolor, Marka, Predkosc, LiczbaKol);
+        }
+    }
+}
